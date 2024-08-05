bf971b1b00b8509be428ed70885dbd0236388bf9
Nitheshkumarms <nitheshkumarms7@gmail.com>
2024-08-05 13:00:42 +0530

    Your commit message

---
+++ cAlogorithms/quickSort.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Function to swap two elements
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// Function to partition the array using a pivot
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; // pivot
+    int i = (low - 1); // Index of smaller element
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+// Function to implement quicksort
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[10];
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    srand(time(0));
+    for (int i = 0; i < arr_size; i++) {
+        arr[i] = rand() % 100;
+    }
+
+    printf("Given array is \n");
+    printArray(arr, arr_size);
+
+    clock_t start = clock();
+    quickSort(arr, 0, arr_size - 1);
+    clock_t end = clock();
+
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Execution time: %f seconds\n", time_spent);
+
+    return 0;
+}
